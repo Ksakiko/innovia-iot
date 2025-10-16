f755193cd6a180f7ac54d8062d0562929f35957a
Sakiko Kawahara <sakikokawahar@gmail.com>
2025-10-16 16:16:40 +0200

    Add frontend localhost to cors setting in Realtime.Hub

--- src/Realtime.Hub/Program.cs
+++ src/Realtime.Hub/Program.cs
@@ -8,7 +8,8 @@ builder.Services.AddCors(opt =>
     {
         policy.WithOrigins(
                 "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "http://localhost:5500",
+                "http://localhost:4200"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
